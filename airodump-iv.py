674ef584102ba9ffb097b9f1897f964c87250f6d
Ivan Leichtling <ivanlei@yelp.com>
2013-04-07 18:14:32 -0700

    Allow channel switching via IOCTL to improve switch speed; also cache available channels via IOCTL

--- airodump-iv.py
+++ airodump-iv.py
@@ -6,13 +6,9 @@ airmon-ng and airodump-ng into a single easily extendable python tool.
 
 Current functionality mimics airodump-ng for the most part.
 
-Currently the code is shelling out to Wireless Tools (WT) to control things
-Should look at using Wireless Extensions (WE) ioctls for speed
+Using Wireless Extensions (WE) ioctls for speedy channel changing
   WT -- http://www.hpl.hp.com/personal/Jean_Tourrilhes/Linux/Tools.html
   WE -- /usr/include/linux/wireless.h
-  Netdevice manual -- http://linux.die.net/man/7/netdevice
-  Good python WE example -- http://pythonwifi.wikispot.org/
-
 """
 import curses
 import errno
@@ -41,6 +37,10 @@ from scapy.fields import LEFieldLenField
 from scapy.fields import LEIntField
 from scapy.fields import LELongField
 from scapy.fields import LEShortField
+from scapy.fields import LESignedIntField
+from scapy.fields import PacketField
+from scapy.fields import PacketLenField
+from scapy.fields import PacketListField
 from scapy.fields import ShortField
 from scapy.fields import StrField
 from scapy.fields import StrFixedLenField
@@ -71,6 +71,16 @@ def shell_command(cmd):
 	del in_mem_file
 	return stdout
 
+class SignedByteField(Field):
+	def __init__(self, name, default):
+		Field.__init__(self, name, default, '<b')
+
+
+class LESignedShortField(Field):
+	def __init__(self, name, default):
+		Field.__init__(self, name, default, '<h')
+
+
 class iw_param(Packet):
 	name = 'iw_param struct'
 
@@ -91,6 +101,25 @@ class iw_point(Packet):
 		LEShortField('flags', 0)
 	]
 
+class iw_quality(Packet):
+	name = 'iw_quality struct'
+
+	fields_desc = [
+		ByteField('qual', 0),
+		ByteField('level', 0),
+		ByteField('noise', 0),
+		ByteField('updated', 0)
+	]
+
+class iw_freq(Packet):
+	name = 'iw_freq struct'
+
+	fields_desc = [
+		LESignedIntField('m', 0),
+		LESignedShortField('e', 0),
+		ByteField('i', 0),
+		ByteField('flags', 0)
+	]
 
 class iw_req(Packet):
 	name = 'iw_req struct'
@@ -100,11 +129,79 @@ class iw_req(Packet):
 	]
 
 
+class iw_freq_req_resp(Packet):
+	name = 'iw_freq_req_resp struct'
+
+	fields_desc = [
+		StrFixedLenField('iface', 0, length=16),
+		PacketLenField('iw_freq', None, iw_freq, length_from=lambda pkt: 8)
+	]
+
+
+class iw_range(Packet):
+	name = 'iw_range struct'
+
+	fields_desc = [
+		LEIntField('throughput', 0),
+		LEIntField('min_nwid', 0),
+		LEIntField('max_nwid', 0),
+		LEShortField('old_num_channels', 0),
+		ByteField('old_num_frequency', 0),
+		ByteField('scan_capa', 0),
+		FieldListField('event_capa', None, LEIntField('', 0), count_from=lambda pkt: 6),
+		LESignedIntField('sensitivity', 0),
+		PacketLenField('max_qual', None, iw_quality, length_from=lambda pkt: 4),
+		PacketLenField('avg_qual', None, iw_quality, length_from=lambda pkt: 4),
+		ByteField('num_bitrates', 0),
+		FieldListField('bitrate', None, LESignedIntField('', 0), count_from=lambda pkt: 32),
+		LESignedIntField('min_rts', 0),
+		LESignedIntField('max_rts', 0),
+
+		LESignedIntField('min_frag', 0),
+		LESignedIntField('max_frag', 0),
+
+		LESignedIntField('min_pmp', 0),
+		LESignedIntField('max_pmp', 0),
+		LESignedIntField('min_pmt', 0),
+		LESignedIntField('max_pmt', 0),
+
+		LEShortField('pmp_flags', 0),
+		LEShortField('pmt_flags', 0),
+		LEShortField('pm_capa', 0),
+		FieldListField('encoding_size', None, LEShortField('', 0), count_from=lambda pkt: 8),
+		ByteField('num_encoding_sizes', 0),
+		ByteField('max_encoding_tokens', 0),
+		ByteField('encoding_login_index', 0),
+		LEShortField('txpower_capa', 0),
+		ByteField('num_txpower', 0),
+		FieldListField('txpower', None, LESignedIntField('', 0), count_from=lambda pkt: 8),
+		ByteField('we_version_compiled', 0),
+		ByteField('we_version_source', 0),
+
+		LEShortField('retry_capa', 0),
+		LEShortField('retry_flags', 0),
+		LEShortField('r_time_flags', 0),
+		LESignedIntField('min_retry', 0),
+		LESignedIntField('max_retry', 0),
+		LESignedIntField('min_r_time', 0),
+		LESignedIntField('max_r_time', 0),
+
+		LEShortField('num_channels', 0),
+		ByteField('num_frequency', 0),
+		FieldListField('freq', None, PacketLenField('', None, iw_freq, length_from=lambda pkt: 8), count_from=lambda pkt: 32),
+		LEIntField('enc_capa', 0)
+	]
+
 class WirelessExtension:
 	IFNAMSIZ = 16
 
+	freq_map = None
+
 	@staticmethod
-	def SIOCGIWRANGE(iface):
+	def ioctl_get_freq_map(iface):
+		if WirelessExtension.freq_map:
+			return WirelessExtension.freq_map
+
 		# Allocate an array for the response
 		buff_out = array.array('B', '\0' * 2048)
 		pointer, length = buff_out.buffer_info()
@@ -115,7 +212,37 @@ class WirelessExtension:
 		sockfd.close()
 		del sockfd
 
-		print repr(buff_out)
+		resp = iw_range(buff_out.tostring())
+
+		WirelessExtension.freq_map = {}
+		for freq in resp.getfieldval('freq'):
+			if freq.m:
+				WirelessExtension.freq_map[str(freq.i)] = freq
+		return WirelessExtension.freq_map
+
+	@staticmethod
+	def ioctl_get_channel(iface):
+		req = iw_freq_req_resp(iface)
+		buff_in_out = array.array('B', str(req))
+		sockfd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		ioctl(sockfd.fileno(), 0x8B05, buff_in_out, 1)
+		sockfd.close()
+		del sockfd
+
+		resp = iw_freq_req_resp(buff_in_out.tostring())
+		return min(14, max(1, (resp[iw_freq].m - 2407) / 5))
+
+	@staticmethod
+	def ioctl_set_channel(iface, channel):
+		WirelessExtension.ioctl_get_freq_map(iface)
+		freq = WirelessExtension.freq_map[str(channel)]
+
+		req = iw_req(iface)/iw_freq(m=freq.m, e=freq.e, i=freq.i, flags=freq.flags)/iw_freq()
+		buff_in_out = array.array('B', str(req))
+		sockfd = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+		ioctl(sockfd.fileno(), 0x8B04, buff_in_out, 0)
+		sockfd.close()
+		del sockfd
 
 class Printer:
 	"""A class for printing messages that respects verbosity levels"""
@@ -164,12 +291,6 @@ def scapy_fields_FieldListField_i2repr(self, pkt, x):
 	return repr([self.field.i2repr(pkt, v) for v in x])
 FieldListField.i2repr = scapy_fields_FieldListField_i2repr
 del scapy_fields_FieldListField_i2repr
-
-
-class SignedByteField(Field):
-	def __init__(self, name, default):
-		Field.__init__(self, name, default, '<b')
-
 
 class ChannelFromMhzField(LEShortField):
 	def m2i(self, pkt, x):
@@ -402,10 +523,10 @@ class Dot11ScannerOptions:
 				iwlist_output = shell_command('iwlist {0} channel'.format(scanner_options.iface))
 				match = re.match('\s?{0}\s+(\d+)'.format(scanner_options.iface), iwlist_output)
 				scanner_options.max_channel = int(match.group(1))
-				Printer.verbose('CHAN: max_channel[{0}]'.format(channel), verbose_level=3)
+				Printer.verbose('CHAN: max_channel[{0}]'.format(scanner_options.max_channel), verbose_level=1)
 			except Exception, e:
 				Printer.exception(e)
-				scanner_options.max_channel = 3
+				raise
 
 		scanner_options.packet_count = options.packet_count
 		scanner_options.input_file = options.input_file
@@ -583,6 +704,8 @@ class Dot11Scanner:
 			# Verify the RadioTap header
 			if packet.haslayer(RadioTap):
 				assert (self.scanner_options.input_file or (self.scanner_options.channel == packet[RadioTap].Channel)), 'got[{0}] expect[{1}]'.format(packet[RadioTap].Channel, self.scanner_options.channel)
+				channel_from_ioctl = WirelessExtension.ioctl_get_channel(self.scanner_options.iface)
+				assert (self.scanner_options.input_file or (self.scanner_options.channel == channel_from_ioctl)), 'got[{0}] expected[{1}]'.format(channel_from_ioctl, self.scanner_options.channel)
 
 			# Track AP and STA
 			if packet.haslayer(Dot11):
@@ -655,8 +778,7 @@ class Dot11Scanner:
 
 	def set_channel(self, channel):
 		Printer.verbose('CHAN: set_channel {0}'.format(channel), verbose_level=3)
-		# TODO(ivanlei): shelling out is slow - investigate using an ioctl
-		shell_command('iwconfig {0} channel {1}'.format(self.scanner_options.iface, channel))
+		WirelessExtension.ioctl_set_channel(self.scanner_options.iface, channel)
 		if self.display:
 			self.display.update_header()
 
@@ -748,4 +870,3 @@ def main():
 
 if __name__ == '__main__':
 	main()
-	#WirelessExtension.SIOCGIWRANGE('mon0')
