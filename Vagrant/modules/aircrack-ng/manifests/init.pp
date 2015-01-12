1d099d0ccaa3c71debfa7dc2032e41b44a64ea6b
Ivan Leichtling <ivanlei@yelp.com>
2015-01-11 20:24:26 -0800

    Use official debian repo for aircrack-ng
    This fixes issue #1.

--- Vagrant/modules/aircrack-ng/manifests/init.pp
+++ Vagrant/modules/aircrack-ng/manifests/init.pp
@@ -3,30 +3,12 @@
 # This class installs aircrack-ng tools
 #
 # Actions:
-#   - Add the unofficial aircrack-ng repo to sources.list
-#   - Allow apt-get install to pull from repos without a key
-#       This is necessary as the aircrack-ng repo is unkeyed
 #   - Installs the aircrack-ng package
 #
 # Sample Usage:
 #  class { 'aircrack-ng': }
 #
 class aircrack-ng {
-    include apt
-
-    apt::source { 'aircrackng-unstable':
-      location => 'http://repo.degeneratedlabs.net/debian/',
-      repos => 'aircrackng-unstable/',
-      release => '',
-      before => Package['aircrack-ng'],
-    }
-
-    apt::conf { 'aircrackng-unstable' :
-      content => 'APT::Get::AllowUnauthenticated "true";',
-      priority => 70,
-      before => Package['aircrack-ng'],
-    }
-
     package { 'aircrack-ng':
       ensure => latest,
     }
